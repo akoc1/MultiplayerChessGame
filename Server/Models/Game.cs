472b10121692e6e10ad0eb605d6e30e88bf461ce
Arif <72667213+akoc1@users.noreply.github.com>
2025-05-08 13:16:10 +0200

    updated IsFull method and changed type of players

---
+++ Server/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models;
+
+public class Game
+{
+    public string GameCode { get; set; }
+
+    public List<GodotClient> Players { get; set; } = new List<GodotClient>();
+
+    public bool IsFull()
+    {
+        return Players.Count == 2;
+    }
+}
